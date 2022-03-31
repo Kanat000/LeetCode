9ef8cd1ffc57591c1c637a599ce49df8d0fa01d6
Kanat000 <kanapulya007@gmail.com>
2022-03-31 12:22:18 +0600

    second-problem

--- src/Medium/ThreeSumSolution.java
+++ src/Medium/ThreeSumSolution.java
@@ -38,6 +38,9 @@ public class ThreeSumSolution {
             }
         }
         return solutionArr;
+
+
+       //My first-try
        /* List<List<Integer>> solutionArr = new ArrayList<>(2);
         for(int i=0;i<nums.length-2;i++){
             for(int j=i+1;j<nums.length-1;j++){
